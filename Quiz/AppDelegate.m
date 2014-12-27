1808750b711ccc68f59019468e010a96528b1f14
V <vsoft@me.com>
2014-12-27 14:39:19 -0500

    - Initial Commit

--- Quiz/AppDelegate.m
+++ Quiz/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -15,8 +16,14 @@
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{	
+	self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 	// Override point for customization after application launch.
+	self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
+	self.window.rootViewController = self.viewController;
+	[self.window makeKeyAndVisible];
+	
 	return YES;
 }
 
