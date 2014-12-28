875aee47f0b7b3ec83691d05864f3dd8b235ff5d
V <vsoft@me.com>
2014-12-28 12:20:04 -0500

    - Removed few unnecessary lines

--- Quiz/AppDelegate.h
+++ Quiz/AppDelegate.h
@@ -14,7 +14,5 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-@property (strong, nonatomic) ViewController *viewController;
-
 @end
 
--- Quiz/AppDelegate.m
+++ Quiz/AppDelegate.m
@@ -19,9 +19,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {	
 	self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-	// Override point for customization after application launch.
-	self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
-	self.window.rootViewController = self.viewController;
+	
+	self.window.backgroundColor = [UIColor whiteColor];
 	[self.window makeKeyAndVisible];
 	
 	return YES;
